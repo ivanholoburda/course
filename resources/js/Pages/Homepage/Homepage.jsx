292021761a61d63b76dcda2024919ba37d25233d
SofiaDrozd <drozdsofia2005@gmail.com>
2025-05-01 18:17:48 -0900

    footer update

--- resources/js/Pages/Homepage/Homepage.jsx
+++ resources/js/Pages/Homepage/Homepage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { useForm } from '@inertiajs/react';
-import './Homepage.css';
-import mainPhoto from '@/assets/photo/mainPhoto.png';
+import { useForm } from "@inertiajs/react";
+import "./Homepage.css";
+import mainPhoto from "@/assets/photo/mainPhoto.png";
 
 export default function Homepage() {
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -10,13 +10,13 @@ export default function Homepage() {
     const closeModal = () => setIsModalOpen(false);
 
     const { data, setData, post, processing, errors } = useForm({
-        email: '',
-        password: ''
+        email: "",
+        password: "",
     });
 
     const handleLogin = (e) => {
         e.preventDefault();
-        post('/login', {
+        post("/login", {
             onSuccess: () => closeModal(),
         });
     };
@@ -27,11 +27,22 @@ export default function Homepage() {
                 <div className="menu">
                     <div className="menuHide">
                         <i className="fa-solid fa-leaf leaf"></i>
-                        <div className="skinCare" id="main">SkinCare</div>
+                        <div className="skinCare" id="main">
+                            SkinCare
+                        </div>
 
                         <div className="menuButtons">
-                            <button onClick={() => window.location.href = '/register'} className="registration">Реєстрація</button>
-                            <button onClick={openModal} className="logIn">Вхід</button>
+                            <button
+                                onClick={() =>
+                                    (window.location.href = "/register")
+                                }
+                                className="registration"
+                            >
+                                Реєстрація
+                            </button>
+                            <button onClick={openModal} className="logIn">
+                                Вхід
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -41,13 +52,19 @@ export default function Homepage() {
                 <div className="includeMain">
                     <div className="phraceMain">Скануй та слідкуй!</div>
                     <div>
-                        Допоможемо вам залишатися гарними та здоровими, дбаючи про терміни
-                        придатності Вашої косметики. За допомогою SkinCare ви зможете легко
-                        відстежувати дати придатності кожного засобу, отримувати сповіщення
-                        про наближення строку та завжди мати під контролем свої косметичні
+                        Допоможемо вам залишатися гарними та здоровими, дбаючи
+                        про терміни придатності Вашої косметики. За допомогою
+                        SkinCare ви зможете легко відстежувати дати придатності
+                        кожного засобу, отримувати сповіщення про наближення
+                        строку та завжди мати під контролем свої косметичні
                         засоби.
                     </div>
-                    <button onClick={() => setIsModalOpen(true)} className="buttonEnter">Ввести продукт</button>
+                    <button
+                        onClick={() => setIsModalOpen(true)}
+                        className="buttonEnter"
+                    >
+                        Ввести продукт
+                    </button>
                 </div>
 
                 <img
@@ -85,38 +102,41 @@ export default function Homepage() {
 
                 <div>
                     <div>2. Додавання продуктів:</div>
-                    <div>Відскануйте штрих-код вашого засобу, або введіть вручну номер</div>
+                    <div>
+                        Відскануйте штрих-код вашого засобу, або введіть вручну
+                        номер
+                    </div>
                 </div>
 
                 <div>
                     <div>3. Сповіщення:</div>
                     <div>
-                        SkinCare буде нагадувати вам про важливі дати, допомагаючи уникнути
-                        використання застарілих продуктів.
+                        SkinCare буде нагадувати вам про важливі дати,
+                        допомагаючи уникнути використання застарілих продуктів.
                     </div>
                 </div>
             </div>
 
-            <footer className="footer">
-                <div className="footerInclude">
-                    <div className="footerLogo">
+            <footer className="footerHome">
+                <div className="footerIncludeHome">
+                    <div className="footerLogoHome">
                         <i className="fa-solid fa-leaf leaf2"></i>
-                        <div className="footerSKin">SkinCare</div>
+                        <div className="footerSKinHome">SkinCare</div>
                     </div>
 
-                    <div className="footerСonnection">
-                        <div className="footerPhone">
+                    <div className="footerСonnectionHome">
+                        <div className="footerPhoneHome">
                             <i className="fa-regular fa-phone"></i>
                             <div className="phone">+380xxxxxxxxx</div>
                         </div>
 
-                        <div className="footerEmail">
+                        <div className="footerEmailHome">
                             <i className="fa-regular fa-envelope"></i>
                             <div className="email">skinscancare@gmail.com</div>
                         </div>
                     </div>
 
-                    <div className="socialNet">
+                    <div className="socialNetHome">
                         <i className="fa-brands fa-telegram telegram"></i>
                         <i className="fa-brands fa-instagram instagram"></i>
                         <i className="fa-brands fa-facebook facebook"></i>
@@ -126,7 +146,10 @@ export default function Homepage() {
 
             {isModalOpen && (
                 <>
-                    <div className="modal-overlay-log" onClick={closeModal}></div>
+                    <div
+                        className="modal-overlay-log"
+                        onClick={closeModal}
+                    ></div>
                     <div className="modalLog">
                         <form onSubmit={handleLogin}>
                             <div className="vhid">Вхід</div>
@@ -135,24 +158,42 @@ export default function Homepage() {
                                 type="email"
                                 placeholder="Логін"
                                 value={data.email}
-                                onChange={(e) => setData('email', e.target.value)}
+                                onChange={(e) =>
+                                    setData("email", e.target.value)
+                                }
                             />
-                            {errors.email && <div className="error">{errors.email}</div>}
+                            {errors.email && (
+                                <div className="error">{errors.email}</div>
+                            )}
 
                             <input
                                 type="password"
                                 placeholder="Пароль"
                                 value={data.password}
-                                onChange={(e) => setData('password', e.target.value)}
+                                onChange={(e) =>
+                                    setData("password", e.target.value)
+                                }
                             />
-                            {errors.password && <div className="error">{errors.password}</div>}
+                            {errors.password && (
+                                <div className="error">{errors.password}</div>
+                            )}
 
-                            <button className="notLogIn" type="button" onClick={() => window.location.href = '/register'}>
+                            <button
+                                className="notLogIn"
+                                type="button"
+                                onClick={() =>
+                                    (window.location.href = "/register")
+                                }
+                            >
                                 Немає акаунту?
                             </button>
 
-                            <button id="submitBtnLog" type="submit" disabled={processing}>
-                                {processing ? 'Вхід...' : 'Увійти'}
+                            <button
+                                id="submitBtnLog"
+                                type="submit"
+                                disabled={processing}
+                            >
+                                {processing ? "Вхід..." : "Увійти"}
                             </button>
                         </form>
                     </div>
